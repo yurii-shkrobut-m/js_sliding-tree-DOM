a46e59ad67bc1478688635be1c18a2c0a4a1dc0d
Yurii Shkrobut <georgebest2409@gmail.com>
2024-06-04 11:44:05 +0300

    make build

---
+++ index.209a81b3.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("li").forEach(function(e){var n=document.createElement("span");e.prepend(n),n.append(n.nextSibling)}),document.body.addEventListener("click",function(e){var n=e.target.nextSibling;n&&(n.hidden=!n.hidden)});
+//# sourceMappingURL=index.209a81b3.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const liItems = document.querySelectorAll("li");
+liItems.forEach((item)=>{
+    const span = document.createElement("span");
+    item.prepend(span);
+    span.append(span.nextSibling);
+});
+document.body.addEventListener("click", (e)=>{
+    const toHidden = e.target.nextSibling;
+    if (toHidden) toHidden.hidden = !toHidden.hidden;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
